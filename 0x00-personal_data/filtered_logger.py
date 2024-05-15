7ab2440962be352293ec74e0252f55cd83794075
Leon Gebre <gelf.learing@gmail.com>
2024-05-15 11:19:44 +0300

    fixing pycodestle and docs

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,12 +1,18 @@
 #!/usr/bin/env python3
-""" this is function called filter_datum that returns the log message obfuscated """
+""" this is function called filter_datum
+    that returns the log message obfuscated
+"""
 import re
 import logging
 
 
-def filter_datum(fields: str, redaction: str, message: str, separator: str) -> str:
+def filter_datum(
+                fields: str, redaction: str, message: str, separator: str
+                ) -> str:
     """ returns the log message obfuscated """
     for field in fields:
-        message = re.sub(rf'{field}=(.*?){separator}',
-                            f'{field}={redaction}{separator}', message)
+        message = re.sub(
+            rf'{field}=(.*?){separator}',
+            f'{field}={redaction}{separator}', message
+        )
     return message
