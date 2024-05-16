f04e13fe0c26f3b132097adc939eb9fb8eac54be
Leon Gebre <gelf.learing@gmail.com>
2024-05-16 15:43:20 +0300

    recuing the line again

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -6,13 +6,10 @@ import re
 import logging
 
 
-def filter_datum(
-                fields: str, redaction: str, message: str, separator: str
-                ) -> str:
+def filter_datum(fields: str, redaction: str, message: str, separator: str) -> str:
     """ returns the log message obfuscated """
     for field in fields:
-        message = re.sub(rf'{field}=(.*?){separator}',
-                        f'{field}={redaction}{separator}', message)
+        message = re.sub(rf'{field}=(.*?){separator}',f'{field}={redaction}{separator}', message)
     return message
 
 
