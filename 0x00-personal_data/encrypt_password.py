3f8483cbafec677ce0a5a09d079349bd7b72c91b
Leon Gebre <gelf.learing@gmail.com>
2024-05-16 22:25:44 +0300

    checing the validy of the password

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -11,3 +11,15 @@ def hash_password(password: str) -> bytes:
     pass_hashed = bcrypt.hashpw(pass_encoded, bcrypt.gensalt())
 
     return pass_hashed
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    '''
+       Use bcrypt to validate that the provided password matches the hashed
+       password.
+    '''
+    valid = False
+    pass_encoded = password.encode()
+    if bcrypt.checkpw(pass_encoded, hashed_password):
+        valid = True
+    return valid
