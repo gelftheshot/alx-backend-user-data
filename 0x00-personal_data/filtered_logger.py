b1146c1f685c1b2c45274502477b7d1f0313ead2
Leon Gebre <gelf.learing@gmail.com>
2024-05-16 15:28:30 +0300

    changing the formater

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -16,3 +16,20 @@ def filter_datum(
             f'{field}={redaction}{separator}', message
         )
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: list, fmat: str = FORMAT):
+        super(RedactingFormatter, self).__init__(fmat)
+        self.fields = fields
+        self.fmat = fmat
+
+    def format(self, record: logging.LogRecord) -> str:
+        return filter_datum(
+            self.fields, self.REDACTION,
+            super().format(record), self.SEPARATOR
+        )
