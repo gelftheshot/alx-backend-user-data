c13eb17b582caad5b5122bb785baacab4d05ef56
Leon Gebre <gelf.learing@gmail.com>
2024-05-20 20:21:11 +0300

    some requirment

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -26,8 +26,8 @@ def stats() -> str:
     return jsonify(stats)
 
 
-@app_views.route('/unauthorized')
-def unauthorized_route() -> str:
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
+def unauthorized() -> str:
     """
         way to return unauthorize end point
     """
