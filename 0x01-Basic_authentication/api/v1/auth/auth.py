f3beeccdb70d110690870c6ddabc5b5d5dfad14a
Leon Gebre <gelf.learing@gmail.com>
2024-05-20 21:28:13 +0300

    modifying requre with method

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -9,9 +9,17 @@ class Auth():
     """the main authentication class
     """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """ retun just fals for now
-        """
-        return False
+        if path is None:
+            return True
+        if excluded_paths is None or len(excluded_paths) == 0:
+            return True
+        path = path + '/' if not path.endswith('/') else path
+        excluded_paths = [
+            p + '/' if not p.endswith('/') else p
+            for p in excluded_paths]
+        if path in excluded_paths:
+            return False
+        return True
 
     def authorization_header(self, request: request = None) -> str:
         """ return none for now it will be
