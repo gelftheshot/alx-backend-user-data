85f55427e93242d1627e5855c96ff925012f04dc
Leon Gebre <gelf.learing@gmail.com>
2024-05-20 22:36:59 +0300

    returning the base64part of encodeing

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -9,4 +9,16 @@ class BasicAuth(Auth):
     """ empty class for now
         but will be modifed later
     """
-    pass
+
+    def extract_base64_authorization_header(
+            self, authorization_header: str) -> str:
+        """ return the the base64 part of
+            header for basic authentication
+        """
+        if authorization_header is None:
+            return None
+        elif not isinstance(authorization_header, str):
+            return None
+        elif not (authorization_header[0:6] == "Basic "):
+            return None
+        return authorization_header[6:]
