96f8a6d12097efbeae6a3a067010852a07945381
Leon Gebre <gelf.learing@gmail.com>
2024-05-21 19:30:58 +0300

    finding and returning email adress

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -4,6 +4,7 @@
 """
 from api.v1.auth.auth import Auth
 import base64
+from models import User
 
 
 class BasicAuth(Auth):
@@ -55,3 +56,27 @@ class BasicAuth(Auth):
         email_and_pass = decoded_base64_authorization_header.split(':', 1)
 
         return email_and_pass[0], email_and_pass[1]
+    def user_object_from_credentials(self, user_email: str, user_pwd: str) -> TypeVar('User'):
+        """return user data based on user name
+        and password
+        """
+        if user_email is None:
+            return None
+        if user_pwd is None:
+            return None
+                if user_email is None or not isinstance(user_email, str):
+            return None
+
+        if user_pwd is None or not isinstance(user_pwd, str):
+            return None
+
+        try:
+            found_users = User.search({'email': user_email})
+        except Exception:
+            return None
+
+        for user in found_users:
+            if user.is_valid_password(user_pwd):
+                return user
+
+        return None
