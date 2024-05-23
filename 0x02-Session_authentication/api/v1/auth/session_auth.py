2b11ee52c1f306951e1d54453a758cec9c656e78
Leon Gebre <gelf.learing@gmail.com>
2024-05-23 08:45:36 +0300

    adding session authentication

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+    session authenticatim mechanizems
+"""
+from api.v1.auth.auth import Auth
+class SessionAuth(Auth):
+    """ empty for know i don't knw what to do
+    """
+    pass
