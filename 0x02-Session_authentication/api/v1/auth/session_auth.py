10c00274d89fbfd4e6de318cf0832e5a4949077c
Leon Gebre <gelf.learing@gmail.com>
2024-05-23 08:55:34 +0300

    creatign a session if for user id

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -3,9 +3,21 @@
     session authenticatim mechanizems
 """
 from api.v1.auth.auth import Auth
-
+from uuid import uuid4
 
 class SessionAuth(Auth):
     """ empty for know i don't knw what to do
     """
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """ a method that will creat a sesstion id for
+            user id
+        """
+        if user_id is None:
+            return None
+        if not isinstance(user_id, str):
+            return None
+        ses_id = str(uuid4())
+        self.user_id_by_session_id[ses_id] = user_id
+        return ses_id
