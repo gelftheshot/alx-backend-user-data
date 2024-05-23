16def06f9da6fa734c1aa3d51d21b221485f8a5b
Leon Gebre <gelf.learing@gmail.com>
2024-05-23 20:42:59 +0300

    my json was bound by string and i just fixed it

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -1,13 +1,10 @@
 #!/usr/bin/env python3
 """ view for users
 """
-from flask import jsonify, abort, make_response
+from flask import jsonify, abort, request
 from api.v1.views import app_views
-from flask import request
 from models.user import User
 from os import getenv
-
-cooki_name = getenv('SESSION_NAME')
 
 
 @app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
@@ -17,16 +14,16 @@ def user_login():
     """
     email = request.form.get('email')
     if not email:
-        return jsonify('{ "error": "email missing" }'), 400
+        return jsonify({ "error": "email missing" }), 400
     user_pwd = request.form.get('password')
     if not user_pwd:
-        return jsonify('{ "error": "password missing" }'), 400
+        return jsonify({ "error": "password missing" }), 400
     try:
         found_users = User.search({'email': email})
     except Exception:
-        return jsonify('{ "error": "no user found for this email" }'), 404
+        return jsonify({ "error": "no user found for this email" }), 404
     if not found_users:
-        return jsonify('{ "error": "no user found for this email" }'), 404
+        return jsonify({ "error": "no user found for this email" }), 404
     c_user = None
     for user in found_users:
         if user.is_valid_password(user_pwd):
@@ -34,7 +31,7 @@ def user_login():
             break
 
     if not c_user:
-        return jsonify('{ "error": "wrong password" }'), 401
+        return jsonify({ "error": "wrong password" }), 401
     else:
         from api.v1.app import auth
 
