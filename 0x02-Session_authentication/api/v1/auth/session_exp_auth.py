cd710e22c6a53d27f428a296a2b01934823725dd
Leon Gebre <gelf.learing@gmail.com>
2024-05-24 11:31:49 +0300

    creating authenticatin duration for sestion

--- 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
@@ -3,10 +3,29 @@
     that is onnce created
 """
 from api.v1.auth.session_auth import SessionAuth
-from os import getenv
+from os
+from datetime import datetime
 
 
-class SessionAuth(SessionAuth):
+class SessionExpAuth(SessionAuth):
     """ the main class fucntion startes here
     """
     def __init__(self):
+        """ constructor fucntion for session
+            authentication expiredate
+        """"
+        session_duration = int(os.environ.get('SESSION_DURATION', 0))
+    
+    def create_session(self, user_id=None):
+        """ over riding the create session class
+            from the parent class
+        """
+        ses_id = super().create_session(user_id)
+        if ses_id is None:
+            return None
+        user_id = self.user_id_for_session_id(ses_id)
+        created_at = datetime.now()
+        session_dict = {"user_id": user_id, "created_at": created_at}
+        self.user_id_by_session_id[ses_id] = session_dict
+        return ses_id
+        
