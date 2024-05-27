f1e9d451f4575b33e83f9bb3cbedd60362445c48
Leon Gebre <gelf.learing@gmail.com>
2024-05-27 10:33:17 +0300

    emplementing find user by method

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -4,6 +4,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.exc import InvalidRequestError, NoResultFound
 
 from user import Base
 from user import User
@@ -37,3 +38,19 @@ class DB:
         self._session.add(new_user)
         self._session.commit()
         return new_user
+ 
+
+    def find_user_by(self, **kwargs):
+        """
+            This method takes in arbitrary keyword arguments
+            and returns the first row found in the users table
+            as filtered by the method’s input arguments
+        """
+        try:
+            user = self._session.query(User).filter_by(**kwargs).one()
+            return user
+        except NoResultFound as e:
+            raise e
+        except InvalidRequestError as e :
+            raise e
+        
