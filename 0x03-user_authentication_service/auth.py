3d0edbeb27f31975907ff38229cd0bbde67f6e88
Leon Gebre <gelf.learing@gmail.com>
2024-05-28 10:40:29 +0300

    updating

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 import bcrypt
 from db import DB
-from user import User
 from sqlalchemy.orm.exc import NoResultFound
+from user import User
 from uuid import uuid4
 
 
@@ -42,27 +42,27 @@ class Auth:
         else:
             raise ValueError(f'User {email} already exists')
 
-    # def valid_login(self, email: str, password: str) -> bool:
-    #     """
-    #         check if the password is correct or not
-    #     """
-    #     try:
-    #         user = self._db.find_user_by(email=email)
-    #         pwd = user.hashed_password
-    #         return bcrypt.checkpw(password.encode('utf-8'), pwd)
-    #     except NoResultFound:
-    #         return False
+    def valid_login(self, email: str, password: str) -> bool:
+        """
+            check if the password is correct or not
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            pwd = user.hashed_password
+            return bcrypt.checkpw(password.encode('utf-8'), pwd)
+        except NoResultFound:
+            return False
 
-    # def create_session(self, email: str) -> str:
-    #     """
-    #         create the session for one user with given
-    #         emial address
-    #     """
-    #     try:
-    #         user = self._db.find_user_by(email=email)
-    #         ses_id = _generate_uuid()
-    #         setattr(user, "session_id", ses_id)
-    #         self._db._session.commit()
-    #         return ses_id
-    #     except NoResultFound:
-    #         return None
+    def create_session(self, email: str) -> str:
+        """
+            create the session for one user with given
+            emial address
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            ses_id = _generate_uuid()
+            setattr(user, "session_id", ses_id)
+            self._db._session.commit()
+            return ses_id
+        except NoResultFound:
+            return None
