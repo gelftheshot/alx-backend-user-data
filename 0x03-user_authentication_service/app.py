d2f93da1ed9127cc5fbb371198d06a408ebd141c
Leon Gebre <gelf.learing@gmail.com>
2024-05-28 10:41:40 +0300

    pycodestyle

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -9,9 +9,11 @@ from auth import Auth
 Auth = Auth()
 app = Flask(__name__)
 
+
 @app.route("/", methods=['GET'], strict_slashes=False)
 def home():
     return jsonify({"message": "Bienvenue"})
+
 
 @app.route("/users", methods=['POST'], strict_slashes=False)
 def users():
@@ -27,6 +29,7 @@ def users():
         return jsonify({"email": f"{email}", "message": "user created"}), 400
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
