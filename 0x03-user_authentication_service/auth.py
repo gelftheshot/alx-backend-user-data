8a8ce9ecc4196e828680092668ee99da11b4a5f1
Leon Gebre <gelf.learing@gmail.com>
2024-05-28 10:50:49 +0300

    changin the way of strin

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -30,17 +30,14 @@ class Auth:
         self._db = DB()
 
     def register_user(self, email: str, password: str) -> User:
-        """
-                a method used to register user
+        """Creates a new user if the email does not exist
         """
         try:
             user = self._db.find_user_by(email=email)
         except NoResultFound:
-            hased_pwd = _hash_password(password)
-            user = self._db.add_user(email, hased_pwd)
-            return user
+            return self._db.add_user(email, _hash_password(password))
         else:
-            raise ValueError(f'User {email} already exists')
+            raise ValueError("User %s already exists" % email)
 
     def valid_login(self, email: str, password: str) -> bool:
         """
