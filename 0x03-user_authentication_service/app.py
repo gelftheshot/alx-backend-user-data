57e5d8c161a27db35f73c9a4ce8c88d32136455a
Leon Gebre <gelf.learing@gmail.com>
2024-05-28 11:57:22 +0300

    logging the user out

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -3,6 +3,7 @@
     a basic flask app
 """
 from flask import Flask, jsonify, request, abort
+from flask import redirect
 from auth import Auth
 AUTH = Auth()
 app = Flask(__name__)
@@ -44,6 +45,19 @@ def login() -> str:
         return response
     else:
         abort(401)
+
+
+@app.route("/sessions", methods=['DELETE'], strict_slashes=False)
+def logout() -> str:
+    """
+        loggin a user out by deleting it's session id
+    """
+    ses_id = request.cookies.get('session_id')
+    user = AUTH.get_user_from_session_id(ses_id)
+    if user:
+        AUTH.destroy_session(ses_id)
+        return redirect('/')
+    return abort(403)
 
 
 if __name__ == "__main__":
