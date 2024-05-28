0b7950664720c55baae0ffc36b62dc3f5e2b8a09
Leon Gebre <gelf.learing@gmail.com>
2024-05-28 10:10:18 +0300

    um rasing noresultfound and invalidrequesterror

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+import bcrypt
+from db import DB
+from user import User
+from sqlalchemy.exc import NoResultFound
+from uuid import uuid4
+
+def _hash_password(password: str) -> bytes:
+    """
+        a method used to hash a password using
+        bcrypt module
+    """
+    salt = bcrypt.gensalt()
+    return bcrypt.hashpw(password.encode('utf-8'), salt)
+
+def _generate_uuid() -> str:
+    """ Generates UUID
+    Returns string representation of new UUID
+    """
+    return str(uuid4())
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+    
+    def register_user(self, email : str, password: str) -> User:
+        """
+                a method used to register user
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            hased_pwd = _hash_password(password)
+            user = self._db.add_user(email, hased_pwd)
+            return user
+        else:
+            raise ValueError(f'User {email} already exists')
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """
+            check if the password is correct or not
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            return False
+        else:
+            return bcrypt.checkpw(password.encode('utf-8'), user.hashed_password)
+
+    def create_session(self, email: str) -> str:
+        """
+            create the session for one user with given
+            emial address
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            ses_id = _generate_uuid()
+            setattr(user, "session_id", ses_id)
+            self._db._session.commit()
+            return ses_id
+        except NoResultFound:
+            return None
+
