99dbfea6483bca40f8d8702b043d72dcee33f06e
Leon Gebre <gelf.learing@gmail.com>
2024-05-28 10:02:30 +0300

    updating pycodestyle

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -34,7 +34,6 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-
     def add_user(self, email: str, hashed_password: str) -> User:
         """ Adds user to database
         Return: User Object
