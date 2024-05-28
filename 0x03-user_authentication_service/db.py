697cb868ee261edf32772b40e4f3cd9c7fdb3aa6
Leon Gebre <gelf.learing@gmail.com>
2024-05-28 09:42:27 +0300

    adding type anonation

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -42,7 +42,7 @@ class DB:
         self._session.commit()
         return user
 
-    def find_user_by(self, **kwargs):
+    def find_user_by(self, **kwargs) -> User:
         """
             This method takes in arbitrary keyword arguments
             and returns the first row found in the users table
